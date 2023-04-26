c8ac7c1fc2de741a2da5d19c80a382b7001b7e51
Chirag Sehgal <chiragsehgalchirag@gmail.com>
2023-04-26 23:50:46 +0530

    Updates

---
+++ precache-manifest.76abf3ba6a15658abca0c9a1090c2d96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4765ebe99dbc533a8cac971c5b94cd4",
+    "url": "/Spacex_App-master/index.html"
+  },
+  {
+    "revision": "c4bb048d5d71f994e1b9",
+    "url": "/Spacex_App-master/static/css/main.90d76699.chunk.css"
+  },
+  {
+    "revision": "56d6ee20d497a4daf4e1",
+    "url": "/Spacex_App-master/static/js/2.e00e8863.chunk.js"
+  },
+  {
+    "revision": "c4bb048d5d71f994e1b9",
+    "url": "/Spacex_App-master/static/js/main.8a6a7e29.chunk.js"
+  },
+  {
+    "revision": "ebb19f50fa4db05f39b2",
+    "url": "/Spacex_App-master/static/js/runtime~main.34439bc5.js"
+  }
+]);
